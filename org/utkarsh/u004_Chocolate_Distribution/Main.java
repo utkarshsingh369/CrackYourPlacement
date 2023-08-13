441ac5ad15cf91486fffe79b3d0caa4b72d33a93
Utkarsh SINGH <utkarsh.singh@amadeus.com>
2023-08-13 19:03:56 +0530

    004 Chocolate Distribution

---
+++ org/utkarsh/u004_Chocolate_Distribution/Main.java
@@ -0,0 +1,22 @@
+package org.utkarsh.u004_Chocolate_Distribution;
+
+import java.util.Arrays;
+
+public class Main {
+    static int distributeChocolate(int[] arr,int n,int m){
+        Arrays.sort(arr);
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<=(n-m);i++){
+            if((arr[i+m-1]-arr[i])<min){
+                min=arr[i+m-1]-arr[i];
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        int[] arr={7, 3, 2, 4, 9, 12, 56};
+        int n=arr.length;
+        int m=3;
+        System.out.println(distributeChocolate(arr,n,m));
+    }
+}
---
+++ org/utkarsh/u004_Chocolate_Distribution/README.md
@@ -0,0 +1,12 @@
+# Number of approach = 1
+
+---
+
+## 1. Optimal Approach
+
+### *[ T: O(nlogn) ; S: O(1) ]*
+
+    1. Sort the array and find the min difference for each range i, i+m-1
+
+## Bonus
+    1. Integer[] arr=a.toArray(new Integer[a.size()]); to convert an arraylist to array of integers.
